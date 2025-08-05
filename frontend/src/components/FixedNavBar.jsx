d5677578059eb3912a0c89e4175aee415e1547a0
ha-taeyeong <electro0218@email.com>
2025-08-05 21:07:37 +0900

    update href

--- frontend/src/components/FixedNavBar.jsx
+++ frontend/src/components/FixedNavBar.jsx
@@ -91,7 +91,7 @@ const FixedNavBar = () => (
         Contact
       </HashLink>
       <a
-        href="http://54.252.56.180:8000/"
+        href="http://54.252.56.180:8000/home"
         target="_blank"
         rel="noopener noreferrer"
         className="
