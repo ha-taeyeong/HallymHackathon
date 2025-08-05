1dc283b3c4d31bbeb49adb14f824225f4ece8979
ha-taeyeong <electro0218@email.com>
2025-08-05 19:32:08 +0900

    rollback

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,55 @@
+// src/components/NavBar.jsx
+import React from "react";
+import { HashLink } from "react-router-hash-link";
+
+const NavBar = () => (
+  <div className="fixed w-full h-[164px] top-0 left-0 bg-white z-50 shadow">
+    <div className="inline-flex items-center justify-end gap-8 absolute top-14 left-[1300px]">
+      <HashLink
+        to="#about"
+        scroll={el => {
+          const y = el.getBoundingClientRect().top + window.pageYOffset - NAV_HEIGHT - EXTRA_GAP;
+          window.scrollTo({ top: y, behavior: "smooth" });
+        }}>
+        About
+      </HashLink>
+
+      <HashLink
+        to="/#skills-section"
+        smooth
+        className="relative w-fit font-body-text font-[number:var(--body-text-font-weight)] 
+          text-black text-[length:var(--body-text-font-size)] tracking-[var(--body-text-letter-spacing)] 
+          leading-[var(--body-text-line-height)] whitespace-nowrap [font-style:var(--body-text-font-style)] cursor-pointer"
+      >
+        Skills
+      </HashLink>
+      <a
+        href="http://54.252.56.180:800/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative w-fit font-body-text font-[number:var(--body-text-font-weight)]  
+          text-black text-[length:var(--body-text-font-size)] tracking-[var(--body-text-letter-spacing)] 
+          leading-[var(--body-text-line-height)] whitespace-nowrap [font-style:var(--body-text-font-style)] cursor-pointer"
+      >
+        Memo
+      </a>
+      <HashLink
+        to="/#contact-section"
+        smooth
+        className="relative w-fit font-body-text font-[number:var(--body-text-font-weight)] 
+          text-black text-[length:var(--body-text-font-size)] tracking-[var(--body-text-letter-spacing)] 
+          leading-[var(--body-text-line-height)] whitespace-nowrap [font-style:var(--body-text-font-style)] cursor-pointer"
+      >
+        Contact
+      </HashLink>
+    </div>
+    <img
+        className="absolute w-[184px] h-[88px] top-[38px] left-[86px] object-cover cursor-pointer"
+        alt="Image"
+        src="https://c.animaapp.com/kPFgkC5m/img/image-21@2x.png"
+        onClick={() => window.location.href = "/"}
+        />
+     </div>
+);
+
+export default NavBar;
